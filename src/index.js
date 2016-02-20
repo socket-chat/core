6588bd7c7834a21503a63b7e64443924b90a7dcd
Kyle Stevenson <kyle@kylestevenson.me>
2016-02-19 20:52:50 -0800

    rename main js file from src/app.js to src/index.js
    remove dotenv and minimist packages. this is because this package
    should be used as a library rather than a script.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import Minimist from 'minimist'
 import SocketServer from 'socket.io'
 
 import { ChatServer } from './chat/ChatServer'
 import * as middleware from './chat/middleware'
-
-require('dotenv').config()
 
 const debug = require('debug')('server')
 
@@ -27,8 +24,8 @@ let createSocketIO = () => {
  * @param  {ChatServer} server {@link ChatServer} instance to bootstrap
  * @return {ChatServer} original instance after bootstrap methods have been applied
  */
-let bootstrapChatServer = (args, server) => {
-  const CONFIG_DIR = args['config-dir'] || (__dirname + '/../config/')
+let bootstrapChatServer = (server) => {
+  const CONFIG_DIR = __dirname + '/../config/'
   let configPath = (path) => CONFIG_DIR + path
 
   // load config
@@ -40,44 +37,18 @@ let bootstrapChatServer = (args, server) => {
   return server
 }
 
-let parseArgs = () => {
-  return Minimist(process.argv.slice(2))
-}
-
 /**
  * Creates a new ChatServer instance
  * @return {ChatServer}
  */
-let createChatServer = (args) => {
+let createChatServer = () => {
   let sio = createSocketIO()
   let server = new ChatServer(sio)
 
-  bootstrapChatServer(args, server)
+  bootstrapChatServer(server)
 
   return server
 }
 
-/**
- * Gets the action (first argument) passed in via the CLI
- * @param  {Minimist} args
- * @return {String} first argument or {@code 'run'} if no action is present
- */
-let parseCliAction = (args) => {
-  return args._[0] || 'run'
-}
-
-let args = parseArgs()
-let chatServer = createChatServer(args)
-
-switch (parseCliAction(args)) {
-  case 'middleware:list':
-    console.log(Object.keys(chatServer.middleware))
-    process.exit()
-    break
-  case 'run':
-    const port = args.port || 8888
-    chatServer.listen(port)
-    debug('listening on port: ' + port)
-    break
-}
-
+createChatServer(args)
+  .listen(8888)
